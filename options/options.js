2e85cf754b0aa17e96e4c08f1e6575e37656565c
Crowdbotics <engineering@crowdbotics.com>
2023-01-20 00:21:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shrill-butterfly-de-58199",
+  url: "https://shrill-butterfly-de-58199.botics.co",
+  api: "https://shrill-butterfly-de-58199.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
